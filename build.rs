f48fe92544bf475dd51c9da9327c967b7a16f37e
Daniel Brotsky <dbrotsky@adobe.com>
2021-06-07 23:25:21 -0700

    Remove the ngl-shim library.
    The sscp library is more compact and tested to be equivalent when it's built.  So we don't the full NGL library anymore.

--- build.rs
+++ build.rs
@@ -10,34 +10,17 @@ fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     if target_os.eq_ignore_ascii_case("macos") {
-        // println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.14");
-        println!("cargo:rustc-link-search=native={}", "rsrc/libraries");
-        println!("cargo:rustc-link-search=native={}", 
-                 "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib");
-        println!(
-            "cargo:rustc-link-search=framework={}",
-            "/System/Library/Frameworks"
-        );
-        println!(
-            "cargo:rustc-link-search=framework={}",
-            "/System/Library/PrivateFrameworks"
-        );
-        println!(
-            "cargo:rustc-link-lib=static={}",
-            format!("sscp-{}", &target_arch)
-        );
-        println!(
-            "cargo:rustc-link-lib=static={}",
-            format!("awg-ngl-{}", &target_arch)
-        );
-        println!("cargo:rustc-link-lib={}", "c++");
-        println!("cargo:rustc-link-lib=framework={}", "Cocoa");
+        let library_path = format!("rsrc/libraries/{}-{}", &target_os, &target_arch);
+        println!("cargo:rustc-link-search=native={}", &library_path);
+        println!("cargo:rustc-link-lib=static={}", "sscp");
+        println!("cargo:rustc-link-lib=dylib={}", "c++");
+        // println!("cargo:rustc-link-lib=framework={}", "Cocoa");
         println!("cargo:rustc-link-lib=framework={}", "CoreFoundation");
-        println!("cargo:rustc-link-lib=framework={}", "CoreServices");
-        println!("cargo:rustc-link-lib=framework={}", "CoreVideo");
+        // println!("cargo:rustc-link-lib=framework={}", "CoreServices");
+        // println!("cargo:rustc-link-lib=framework={}", "CoreVideo");
         println!("cargo:rustc-link-lib=framework={}", "IOKit");
-        println!("cargo:rustc-link-lib=framework={}", "Security");
-        println!("cargo:rustc-link-lib=framework={}", "SystemConfiguration");
-        println!("cargo:rustc-link-lib=framework={}", "Webkit");
+        // println!("cargo:rustc-link-lib=framework={}", "Security");
+        // println!("cargo:rustc-link-lib=framework={}", "SystemConfiguration");
+        // println!("cargo:rustc-link-lib=framework={}", "Webkit");
     }
 }
--- src/client/sscp.rs
+++ src/client/sscp.rs
@@ -11,6 +11,9 @@ use sha2::{Digest, Sha256};
 const FALLBACK_DEVICE_ID: &str =
     "fdaa587852001bad7bf1e81ed275b822d2bc812f4eeeab092da60b7066ab2bfa";
 
+// libsscp is a custom library supplied in various architectures
+// and linked by the build.rs script.  It provides all the
+// entries in this extern block.
 extern "C" {
     fn get_sscp_device_id(buf: *mut u8, len: i32) -> i32;
 }
@@ -27,5 +30,32 @@ pub fn get_device_id() -> String {
             let digest = Sha256::digest(&buf[0..id_len]);
             format!("{:x}", digest)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_device_id;
+    use crate::client::sscp::FALLBACK_DEVICE_ID;
+
+    #[test]
+    fn test_device_id_shape() {
+        let sscp_id = get_device_id();
+        assert_eq!(sscp_id.len(), 64, "SSCP Device ID is not 64 characters");
+        assert!(
+            sscp_id.chars().all(|c| c.is_ascii_hexdigit()),
+            "SSCP Device ID is not all hex digits"
+        );
+    }
+
+    #[test]
+    fn test_device_id_nomatch() {
+        let sscp_id = get_device_id();
+        assert_ne!(
+            FALLBACK_DEVICE_ID,
+            sscp_id.as_str(),
+            "The test device has a fallback device ID"
+        );
+        println!("The test device ID is: {}", sscp_id.as_str());
     }
 }
