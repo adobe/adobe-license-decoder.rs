7a697a0a6f90037deeff35d4ed48e58950ccfd9d
Daniel Brotsky <dbrotsky@adobe.com>
2021-06-17 14:26:34 -0700

    Update to use new SSCP library.
    The SSCP library has been revamped to be tiny, to return actual NGL base hardware IDs, and to do all its own compatibility testing with the NGL library.  So the toolbox now just uses it directly and doesn't bother doing compatibility testing.
    
    This is still a mac-only build.  Link info for Windows to be verified.

--- build.rs
+++ build.rs
@@ -9,12 +9,29 @@ it.
 fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let library_path = format!("rsrc/libraries/{}-{}", &target_os, &target_arch);
+    println!("cargo:rustc-link-search=native={}", &library_path);
     if target_os.eq_ignore_ascii_case("macos") {
-        let library_path = format!("rsrc/libraries/{}-{}", &target_os, &target_arch);
-        println!("cargo:rustc-link-search=native={}", &library_path);
         println!("cargo:rustc-link-lib=static=sscp");
         println!("cargo:rustc-link-lib=dylib=c++");
         println!("cargo:rustc-link-lib=framework=CoreFoundation");
         println!("cargo:rustc-link-lib=framework=IOKit");
+    } else if target_os.eq_ignore_ascii_case("windows") {
+        println!("cargo:rustc-link-lib=static=sscp");
+        println!("cargo:rustc-link-lib=static=ngl-lib");
+        println!("cargo:rustc-link-lib=static=winhttp");
+        println!("cargo:rustc-link-lib=static=kernel32");
+        println!("cargo:rustc-link-lib=static=user32");
+        println!("cargo:rustc-link-lib=static=gdi32");
+        println!("cargo:rustc-link-lib=static=winspool");
+        println!("cargo:rustc-link-lib=static=comdlg32");
+        println!("cargo:rustc-link-lib=static=advapi32");
+        println!("cargo:rustc-link-lib=static=shell32");
+        println!("cargo:rustc-link-lib=static=ole32");
+        println!("cargo:rustc-link-lib=static=oleaut32");
+        println!("cargo:rustc-link-lib=static=uuid");
+        println!("cargo:rustc-link-lib=static=odbc32");
+        println!("cargo:rustc-link-lib=static=odbccp32");
+        println!("cargo:rustc-link-lib=static=libcpmt");
     }
 }
--- src/client/sscp.rs
+++ src/client/sscp.rs
@@ -8,23 +8,25 @@ it.
 */
 use sha2::{Digest, Sha256};
 
-const FALLBACK_DEVICE_ID: &str =
-    "fdaa587852001bad7bf1e81ed275b822d2bc812f4eeeab092da60b7066ab2bfa";
-
 // libsscp is a custom library supplied in various architectures
 // and linked by the build.rs script.  It provides all the
 // entries in this extern block.
-extern "C" {
-    fn get_sscp_device_id(buf: *mut u8, len: i32) -> i32;
+mod libsscp {
+    pub const FALLBACK_NGL_DEVICE_ID: &str =
+        "fdaa587852001bad7bf1e81ed275b822d2bc812f4eeeab092da60b7066ab2bfa";
+
+    extern "C" {
+        pub fn get_ngl_hardware_id(buf: *mut u8, len: i32) -> i32;
+    }
 }
 
-pub fn get_device_id() -> String {
+pub fn get_ngl_device_id() -> String {
     const BUFSIZE: i32 = 512;
     let mut buf: [u8; BUFSIZE as usize] = [0; BUFSIZE as usize];
-    let id_len = unsafe { get_sscp_device_id(buf.as_mut_ptr(), BUFSIZE) };
+    let id_len = unsafe { libsscp::get_ngl_hardware_id(buf.as_mut_ptr(), BUFSIZE) };
     match id_len {
-        0 => FALLBACK_DEVICE_ID.to_string(),
-        -1 => panic!("Underlying Device ID is larger than {} bytes!", BUFSIZE),
+        0 => libsscp::FALLBACK_NGL_DEVICE_ID.to_string(),
+        -1 => panic!("Underlying NGL Device ID is larger than {} bytes!", BUFSIZE),
         _ => {
             let id_len = id_len as usize;
             let digest = Sha256::digest(&buf[0..id_len]);
@@ -35,27 +37,37 @@ pub fn get_device_id() -> String {
 
 #[cfg(test)]
 mod tests {
-    use super::get_device_id;
-    use crate::client::sscp::FALLBACK_DEVICE_ID;
+    use super::get_ngl_device_id;
+    use super::libsscp;
 
     #[test]
     fn test_device_id_shape() {
-        let sscp_id = get_device_id();
-        assert_eq!(sscp_id.len(), 64, "SSCP Device ID is not 64 characters");
+        let ngl_id = get_ngl_device_id();
+        assert_eq!(ngl_id.len(), 64, "NGL Device ID is not 64 characters");
         assert!(
-            sscp_id.chars().all(|c| c.is_ascii_hexdigit()),
+            ngl_id.chars().all(|c| c.is_ascii_hexdigit()),
             "SSCP Device ID is not all hex digits"
         );
     }
 
     #[test]
-    fn test_device_id_nomatch() {
-        let sscp_id = get_device_id();
-        assert_ne!(
-            FALLBACK_DEVICE_ID,
-            sscp_id.as_str(),
-            "The test device has a fallback device ID"
-        );
-        println!("The test device ID is: {}", sscp_id.as_str());
+    fn test_device_id_match_cases() {
+        const BUFSIZE: i32 = 512;
+        let mut buf: [u8; BUFSIZE as usize] = [0; BUFSIZE as usize];
+        let ngl_id = get_ngl_device_id();
+        let sscp_id = unsafe { libsscp::get_ngl_hardware_id(buf.as_mut_ptr(), BUFSIZE) };
+        match sscp_id {
+            0 => assert_eq!(
+                ngl_id,
+                libsscp::FALLBACK_NGL_DEVICE_ID,
+                "Fallback ID was not returned but should have been."
+            ),
+            _ => assert_ne!(
+                ngl_id,
+                libsscp::FALLBACK_NGL_DEVICE_ID,
+                "Fallback ID was returned but should not have been."
+            ),
+        }
+        println!("The test machine NGL device ID is: {}", ngl_id.as_str());
     }
 }
