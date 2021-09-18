d09d1208876adf8528eb5e71bb11c619ed7a8370
Daniel Brotsky <dbrotsky@adobe.com>
2021-09-17 22:08:18 -0700

    Get parsing working for SDL and LAN operating configs.
    Addtionally, changed serialization of the operating configs not to encode sections in Base64, so it's easy to get a complete view of the JSON in an operating config file.
    
    When serializing operating configs that don't have a `profileServerCertFingerprint` element, omit the element completely.  (It's only used in LAN configs.)

--- src/client/cops/oc.rs
+++ src/client/cops/oc.rs
@@ -13,7 +13,7 @@ use serde::{Deserialize, Serialize};
 pub struct OperatingConfig {
     pub oc_spec_version: String,
     pub signatures: Vec<SignatureSpecifier>,
-    #[serde(with = "crate::utilities::base64_encoded_json")]
+    #[serde(deserialize_with = "crate::utilities::base64_encoded_json::deserialize")]
     pub payload: OcPayload,
 }
 
@@ -24,11 +24,13 @@ pub struct OcPayload {
     pub npd_id: String,
     pub ngl_app_id: String,
     pub npd_precedence: i32,
-    pub asnp_data: AsnpData,
+    pub asnp_data: Option<AsnpData>,
     pub profile_server_url: String,
     pub profile_request_payload_params: Option<ProfileRequestPayloadParams>,
     pub deployment_mode: String,
     pub branding: BrandingData,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub profile_server_cert_fingerprint: Option<String>, // LAN only
 }
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
@@ -57,7 +59,6 @@ pub struct ProfileRequestPayloadParams {
 #[serde(rename_all = "camelCase")]
 pub struct AdobeSignedValues {
     pub signatures: AdobeSignatures,
-    // #[serde(with = "crate::utilities::base64_encoded_json")]
     pub values: AdobeValues,
 }
 
@@ -65,7 +66,7 @@ pub struct AdobeSignedValues {
 #[serde(rename_all = "camelCase")]
 pub struct CustomerSignedValues {
     pub signatures: CustomerSignatures,
-    #[serde(with = "crate::utilities::base64_encoded_json")]
+    #[serde(deserialize_with = "crate::utilities::base64_encoded_json::deserialize")]
     pub values: CustomerValues,
 }
 
@@ -121,7 +122,7 @@ pub struct CacheExpiryWarningControl {
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SignatureSpecifier {
-    #[serde(with = "crate::utilities::base64_encoded_json")]
+    #[serde(deserialize_with = "crate::utilities::base64_encoded_json::deserialize")]
     pub header: SignatureHeaderData,
     pub signature: String,
 }
@@ -150,9 +151,11 @@ pub struct CertificateDetails {
 mod tests {
     use super::OperatingConfig;
     use crate::client::types::{FileInfo, OperatingConfig as ManualOperatingConfig};
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_online() {
+        // first parse the actual operating config and make sure it matches the hand parse
         let path = "rsrc/OperatingConfigs/UGhvdG9zaG9wMXt9MjAxODA3MjAwNA-ODU0YjU5OGQtOTE1Ni00NDZiLWFlZDYtMGQ1ZGM2ZmVhZDBi-80.operatingconfig";
         let info = FileInfo::from_path(path).expect("Can't find online test data");
         let json =
@@ -163,7 +166,12 @@ mod tests {
             .expect("Can't manually extract config");
         assert_eq!(oc1.payload.npd_id, oc2.npd_id, "npdIds do not match");
         assert_eq!(oc1.payload.ngl_app_id, oc2.app_id, "appIds do not match");
-        println!("test_online complete!");
+        // now serialize the OC and make sure it matches the reference decode
+        let decode = serde_json::to_string(&oc1).unwrap();
+        let ref_path = "rsrc/OperatingConfigs/ps-online-proxy.operatingconfig";
+        let ref_decode =
+            std::fs::read_to_string(ref_path).expect("Can't read reference JSON");
+        assert_eq!(decode, ref_decode);
     }
 
     #[test]
@@ -178,5 +186,77 @@ mod tests {
             .expect("Can't manually extract config");
         assert_eq!(oc1.payload.npd_id, oc2.npd_id, "npdIds do not match");
         assert_eq!(oc1.payload.ngl_app_id, oc2.app_id, "appIds do not match");
+        let decode = serde_json::to_string(&oc1).unwrap();
+        let ref_path = "rsrc/OperatingConfigs/ai-isolated.operatingconfig";
+        let ref_decode =
+            std::fs::read_to_string(ref_path).expect("Can't read reference JSON");
+        assert_eq!(decode, ref_decode);
+    }
+
+    #[test]
+    fn test_lan() {
+        let path = "rsrc/OperatingConfigs/SWxsdXN0cmF0b3Ixe30yMDE4MDcyMDA0-OTUzZTViZWYtYWJmMy00NGUxLWFjYjUtZmZhN2MyMDY4YjQx-80.operatingconfig";
+        let info = FileInfo::from_path(path).expect("Can't find LAN test data");
+        let json =
+            std::fs::read_to_string(&info.pathname).expect("Can't read LAN data file");
+        let oc1: OperatingConfig =
+            serde_json::from_str(&json).expect("Can't parse LAN data");
+        let oc2 = ManualOperatingConfig::from_license_file(&info)
+            .expect("Can't manually extract config");
+        assert_eq!(oc1.payload.npd_id, oc2.npd_id, "npdIds do not match");
+        assert_eq!(oc1.payload.ngl_app_id, oc2.app_id, "appIds do not match");
+        let decode = serde_json::to_string(&oc1).unwrap();
+        let ref_path = "rsrc/OperatingConfigs/ai-lan.operatingconfig";
+        let ref_decode =
+            std::fs::read_to_string(ref_path).expect("Can't read reference JSON");
+        assert_eq!(decode, ref_decode);
+    }
+
+    #[test]
+    fn test_sdl() {
+        let acro_path = "rsrc/OperatingConfigs/QWNyb2JhdERDMXt9MjAxODA3MjAwNA-NDIzOTc1ZTItODQ2Ni00MDU0LTk2ZDEtNWQ4NzMwOWE4NGZk-90.operatingconfig";
+        let id_path = "rsrc/OperatingConfigs/SW5EZXNpZ24xe30yMDE4MDcyMDA0-NDIzOTc1ZTItODQ2Ni00MDU0LTk2ZDEtNWQ4NzMwOWE4NGZk-90.operatingconfig";
+        let acro_info =
+            FileInfo::from_path(acro_path).expect("Can't find Acrobat SDL test data");
+        let id_info =
+            FileInfo::from_path(id_path).expect("Can't find InDesign SDL test data");
+        let acro_json = std::fs::read_to_string(&acro_info.pathname)
+            .expect("Can't read Acrobat LAN data file");
+        let id_json = std::fs::read_to_string(&id_info.pathname)
+            .expect("Can't read InDesign LAN data file");
+        let acro_oc1: OperatingConfig =
+            serde_json::from_str(&acro_json).expect("Can't parse Acrobat LAN data");
+        let id_oc1: OperatingConfig =
+            serde_json::from_str(&id_json).expect("Can't parse InDesign LAN data");
+        let acro_oc2 = ManualOperatingConfig::from_license_file(&acro_info)
+            .expect("Can't manually extract Acrobat config");
+        let id_oc2 = ManualOperatingConfig::from_license_file(&id_info)
+            .expect("Can't manually extract Acrobat config");
+        assert_eq!(
+            acro_oc1.payload.npd_id, acro_oc2.npd_id,
+            "Acrobat npdIds do not match"
+        );
+        assert_eq!(
+            acro_oc1.payload.ngl_app_id, acro_oc2.app_id,
+            "Acrobat appIds do not match"
+        );
+        assert_eq!(
+            id_oc1.payload.npd_id, id_oc2.npd_id,
+            "InDesign npdIds do not match"
+        );
+        assert_eq!(
+            id_oc1.payload.ngl_app_id, id_oc2.app_id,
+            "InDesign appIds do not match"
+        );
+        let acro_decode = serde_json::to_string(&acro_oc1).unwrap();
+        let acro_ref_path = "rsrc/OperatingConfigs/acrobat-sdl.operatingconfig";
+        let acro_ref_decode =
+            std::fs::read_to_string(acro_ref_path).expect("Can't read reference JSON");
+        assert_eq!(acro_decode, acro_ref_decode);
+        let id_decode = serde_json::to_string(&id_oc1).unwrap();
+        let id_ref_path = "rsrc/OperatingConfigs/indesign-sdl.operatingconfig";
+        let id_ref_decode =
+            std::fs::read_to_string(id_ref_path).expect("Can't read reference JSON");
+        assert_eq!(id_decode, id_ref_decode);
     }
 }
